51d624cad0c5a1da39bbc5759f60e0dbb47a2400
Megan Huggins <Muggins1265@gmail.com>
2020-05-04 21:17:03 -0600

    Initial commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
 import './App.css';
+import { local } from '../../data/local';
+import NewsArticle from .....
 
 class App extends Component {
   constructor() {
@@ -13,10 +15,29 @@ class App extends Component {
   render () {
     return (
       <div className="app">
-        YOUR CODE GOES HERE!
+        // YOUR CODE GOES HERE!
+        {local.map(article => (
+          <NewsArticle article={article} />
+        ))}
       </div>
     );
   }
 }
 
 export default App;
+
+// this would be in another file and imorted into here
+class NewsArticle extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.description}</p>
+    )
+  }
+}
