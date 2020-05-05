3c39180494502bb39e6a6d3e17e92a81e30bb4ad
Megan Huggins <Muggins1265@gmail.com>
2020-05-05 16:03:26 -0600

    Add more styling to the nav bar and main section including the individual cards

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './NewsArticle.css';
 
 const NewsArticle = ({id, headline, img, description, url}) => {
+  const style = {
+    backgroundImage: `url('${img}')`,
+  };
+
   return (
     <div className='card'>
-      <img src={img}/>
+      <div className="card-img" style={style}/>
       <h3>{headline}</h3>
       <p>{description}</p>
       <a href={url}>LINK</a>
