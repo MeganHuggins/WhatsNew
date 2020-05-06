aafda3d3b7008e8c97cee8df1d406756625716b3
Megan Huggins <Muggins1265@gmail.com>
2020-05-05 20:33:02 -0600

    Link other catergory data to App/ render on DOM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
-import local from '../../data/local';
 import NewsContainer from '../NewsContainer/NewsContainer';
 import Header from '../Header/Header';
 import Menu from '../Menu/Menu';
+import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      local,
+      entertainment,
+      health,
+      science,
+      technology,
+      currentCategory: 'local',
     }
-  }
+  };
+
+  updateCategory = (e, str) => {
+    e.preventDefault();
+    this.setState({currentCategory: str})
+  };
 
   render () {
     return (
       <div className="app">
         <Header />
-        <Menu />
-        <NewsContainer news={this.state.local} />
+        <Menu updateCategory={this.updateCategory} />
+        <NewsContainer news={this.state[this.state.currentCategory]} />
       </div>
     );
-  }
+  };
 }
 
 export default App;
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Menu.css'
 
-const Menu = () => {
+const Menu = ({updateCategory}) => {
   return (
     <nav className="nav-bar">
-      <button>Local</button>
-      <button>Health</button>
-      <button>Entertainment</button>
-      <button>Science</button>
-      <button>Technology</button>
+      <button onClick={(e) => updateCategory(e, 'local')}>Local</button>
+      <button onClick={(e) => updateCategory(e, 'health')}>Health</button>
+      <button onClick={(e) => updateCategory(e, 'entertainment')}>Entertainment</button>
+      <button onClick={(e) => updateCategory(e, 'science')}>Science</button>
+      <button onClick={(e) => updateCategory(e, 'technology')}>Technology</button>
+      <div className="circle-one"></div>
+      <div className="circle-two"></div>
+      <div className="circle-three"></div>
     </nav>
   )
 }
