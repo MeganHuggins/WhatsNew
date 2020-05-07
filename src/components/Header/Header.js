a8afd8746969949d7fc9af8cfe78a7c1bd36fee3
Megan Huggins <Muggins1265@gmail.com>
2020-05-06 21:04:02 -0600

    Change header.js to a class component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import './Header.css';
 
-const Header = () => {
-  return (
-    <header className="header">
-      <h1>What's <span>News?</span></h1>
-    </header>
-  )
+class Header extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      searchQuery: ''
+    }
+  }
+
+  updateSearchQuery = (e) => {
+    this.setState({searchQuery: e.target.value.toLowerCase()})
+  }
+
+  searchArticles = (e) => {
+    e.preventDefault();
+    this.props.updateSearchQuery(this.state.searchQuery);
+    this.setState({searchQuery: ''})
+  }
+
+  render() {
+    return (
+      <header className='header'>
+        <h1>What's <span>News?</span></h1>
+        <form>
+          <input
+            value={this.state.searchQuery}
+            type='search'
+            placeholder='Search for article'
+            onChange={this.updateSearchQuery}
+          />
+          <button
+            onClick={this.searchArticles}
+          >
+            Search
+          </button>
+        </form>
+      </header>
+    )
+  }
 }
 
 export default Header;
--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './NewsContainer.css';
 import NewsArticle from '../NewsArticle/NewsArticle';
 
-const NewsContainer = ({news}) => {
+const NewsContainer = ({ news }) => {
   const newsCards = news.map(article => {
     return (
       <NewsArticle
-        id = {article.id}
-        headline = {article.headline}
-        img = {article.img}
-        description = {article.description}
-        url = {article.url}
+        key={article.id}
+        id={article.id}
+        headline={article.headline}
+        img={article.img}
+        description={article.description}
+        url={article.url}
       />
     )
   });
