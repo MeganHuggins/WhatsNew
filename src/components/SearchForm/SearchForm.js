7678355abe059e42761e160ec0f54eef9c8dbb47
Megan Huggins <Muggins1265@gmail.com>
2020-05-07 13:19:48 -0600

    Create test for Header.js rendering

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,43 @@
 import React from 'react';
 import './SearchForm.css';
 
-// SEARCHFORM COMPONENT CODE GOES HERE
+class SearchForm extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      searchQuery: ''
+    }
+  }
+
+  updateSearchQuery = (e) => {
+    this.setState({searchQuery: e.target.value.toLowerCase()})
+  }
+
+  searchArticles = (e) => {
+    e.preventDefault();
+    this.props.updateSearchQuery(this.state.searchQuery);
+    this.setState({searchQuery: ''})
+  }
+
+  render() {
+    return (
+        <form className='search-bar' data-testid='search-bar'>
+          <input
+            value={this.state.searchQuery}
+            type='search'
+            placeholder='Search for article'
+            onChange={this.updateSearchQuery}
+          />
+          <button
+            onClick={this.searchArticles}
+          >
+            Search
+          </button>
+        </form>
+    )
+  }
+}
+
 
 export default SearchForm;
