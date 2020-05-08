7968e9d0bb30d386f221b416df2f910dcd244d70
Megan Huggins <Muggins1265@gmail.com>
2020-05-07 19:00:56 -0600

    Updates

---
+++ precache-manifest.ae471693d7d3b864ca4ca5f758199cc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371b8c8baf890ebd5da65d83e1b75c51",
+    "url": "/MeganHuggins/WhatsNew/index.html"
+  },
+  {
+    "revision": "abd5bf857391f2425666",
+    "url": "/MeganHuggins/WhatsNew/static/css/main.959b0afe.chunk.css"
+  },
+  {
+    "revision": "0af19c1f231165f040b7",
+    "url": "/MeganHuggins/WhatsNew/static/js/2.e4d574d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MeganHuggins/WhatsNew/static/js/2.e4d574d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd5bf857391f2425666",
+    "url": "/MeganHuggins/WhatsNew/static/js/main.4c5a7065.chunk.js"
+  },
+  {
+    "revision": "ba67b2bf0c59728a6079",
+    "url": "/MeganHuggins/WhatsNew/static/js/runtime-main.09c29a63.js"
+  },
+  {
+    "revision": "93aedb6c3238eba48b399e2839cb8116",
+    "url": "/MeganHuggins/WhatsNew/static/media/rocket.93aedb6c.png"
+  }
+]);
